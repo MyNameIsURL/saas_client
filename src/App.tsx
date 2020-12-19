f35cab932d05da4de43bf2e03d917717086e7f88
Patrick Ayres <briceayres@gmail.com>
2020-12-19 00:39:36 -0600

    added navbar and login and register pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { LoginPage } from "./pages/LoginPage";
+import { RegistrationPage } from "./pages/RegistrationPage";
 
 export const App = () => {
   return (
-    <div className="max-w-md rounded overflow-hidden shadow-lg">
-      test
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/register">
+            <RegistrationPage />
+          </Route>
+          <Route path="/">
+            <Redirect to="/login" />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
